7fbe06848febd5b1cb3ca10c9db88df938a68d01
lx <you@example.com>
2020-09-17 22:48:43 +0800

    add config

---
+++ src/config/authConfig.js
@@ -0,0 +1,28 @@
+export let authconfig = {
+
+	HTTPManagement:{
+		ctrName:'主页',
+		iconFont:'iconfont icon-zhuye',
+		url:'/main/index',
+	},
+	productionManagement:{
+		ctrName:'数据查询',
+		iconFont:'iconfont icon-tubiaoshangshengqushi',
+		url:'/main/dashboard',
+	},
+	qualityManagement:{
+		ctrName:'报表分析',
+		iconFont:'iconfont icon-baobiao',
+		url:'/main/report',
+	},
+	notificationManagement:{
+		ctrName:'实时报警',
+		iconFont:'iconfont icon-jinggao',
+		url:'/main/warning',
+	},
+	userManagement:{
+		ctrName:'用户管理',
+		iconFont:'iconfont icon-yonghu1',
+		url:'/main/user',
+	},
+}
---
+++ src/config/ipConfig.js
@@ -0,0 +1,5 @@
+export let ipConfig = {
+
+
+	//url	: 'http://192.168.3.15:8000',
+}
