721f98cb1f02c693f0e10487e62a36eec0e06849
lx <you@example.com>
2020-09-19 17:15:50 +0800

    chartdatas

--- src/components/index.vue
+++ src/components/index.vue
@@ -19,9 +19,8 @@
 			</div>
 		</div>
 		<div class="rightbox">
-      <span>{{date}}</span>
-      </br>
-      <span>{{tem1}}</span>
+<!--       <span>{{date}}</span></br>
+      <span>{{tem1}}</span> -->
     </div>
     <div class="chart" id="chart"></div>
 	</div>
@@ -55,46 +54,36 @@
                 useUTC: false
             }
         });
-        this.onGetchart();
+        this.getWeather();
+        //this.onGetchart();
 		},
 		methods:{
 			getWeather() {
 				this.$axios.get('https://www.tianqiapi.com/api/?appid=91536352 &appsecret=Py9Zs0RD&version=v1&cityid=310100' ).then((reponse) =>{
 					this.tableData = reponse.data.data;
-          this.day[0] = this.tableData[0].day;
-          this.day[1] = this.tableData[1].day;
-          this.day[2] = this.tableData[2].day;
-          this.day[3] = this.tableData[3].day;
-          this.day[4] = this.tableData[4].day;
-          this.day[5] = this.tableData[5].day;
-          this.day[6] = this.tableData[6].day;
-          this.date[0] = this.tableData[0].date;
-          this.date[1] = this.tableData[1].date;
-          this.date[2] = this.tableData[2].date;
-          this.date[3] = this.tableData[3].date;
-          this.date[4] = this.tableData[4].date;
-          this.date[5] = this.tableData[5].date;
-          this.date[6] = this.tableData[6].date;
-          this.tem1[0] = parseInt(this.tableData[0].tem1);
-          this.tem1[1] = parseInt(this.tableData[1].tem1);
-          this.tem1[2] = parseInt(this.tableData[2].tem1);
-          this.tem1[3] = parseInt(this.tableData[3].tem1);
-          this.tem1[4] = parseInt(this.tableData[4].tem1);
-          this.tem1[5] = parseInt(this.tableData[5].tem1);
-          this.tem1[6] = parseInt(this.tableData[6].tem1);
-					console.log(this.tem1);
+          for(var i=0;i<7;i++){
+            this.date[i] = this.tableData[i].date
+            this.tem1[i] = parseInt(this.tableData[i].tem1)
+          }
+          console.log(this.tem1);
           console.log(this.date);
+          //console.log(this.date[2])
+          this.onGetchart();
 				},function(reponse){
 					console.log("test");
 					}).catch(() => {})
 			},
       onGetchart(){
-        this.getWeather();
+        console.log(this.tem1);
+        console.log(this.date);
+        console.log(this.date[2]);
         this.chart = Highchart.chart('chart', {
           xAxis: {
+            type: 'datetime',
+            title: {
+              text :null
+            },
             categories: this.date,
-            //categories: [ "2020-09-18", "2020-09-19", "2020-09-20", "2020-09-21", "2020-09-22", "2020-09-23", "2020-09-24" ],
-            //categories: [this.day[0], this.day[1], this.day[2],this.day[3], this.day[4], this.day[5],this.day[6]],
           },
           yAxis: {
             title: {
@@ -110,12 +99,13 @@
             }
           },
           credits:{enabled:false,},
+          tooltip: {
+              //headerFormat: '<b>{xAxis.categories}</b><br>',
+              pointFormat: ' {point.y:.0f} ℃'
+          },
           series: [{
             showInLegend: false,
-            //data: [ 23, 23, 28, 26, 21, 23, 25 ],
             data: this.tem1,
-            //data: [20, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, ]
-            //data: [parseInt(this.tem1[0]),parseInt(this.tem1[1]),parseInt(this.tem1[2]),parseInt(this.tem1[3]),parseInt(this.tem1[4]),parseInt(this.tem1[5]),parseInt(this.tem1[6])]
           }]
         });
       }
